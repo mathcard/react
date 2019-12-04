2b5b9d9b55c53bd8f98566aadcbcd234a1d272a9
mathcard <matheus.carmo@hotmail.com>
2019-12-04 13:59:44 -0300

    Iniciando estudos no git hub

---
+++ hunt/AppBkp.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Comment{
+  render(){
+    return(
+      <View styles={styles.container}>
+        <Text styles={styles.welcome}> Welcome to React Native</Text>       
+        <View styles={styles.box}>
+        </View> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: '20',
+    textAlign: 'center',
+    margin: 10,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#F00',
+  },
+});
---
+++ hunt/src/pages/main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Main extends Component{
+    static navigationOptions = {
+        title: "JS Hunt"
+    };
+
+    render(){
+        return(
+            <View>
+                <Text> Página Main</Text>
+            </View>
+        );
+    }
+}
