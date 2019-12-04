7c8fa68d3e1738f90aa8e3b4ff8686b1747d0cd5
mathcard <matheus.carmo@hotmail.com>
2019-12-04 19:08:43 -0300

    APi

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component{
     static navigationOptions = {
         title: "JS Hunt"
+    };
+
+    componentDidMount(){ // Disparado automaticamente
+        this.loadProducts();        
+    }
+
+    loadProducts = () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+        
+        console.log(docs);
     };
 
     render(){
