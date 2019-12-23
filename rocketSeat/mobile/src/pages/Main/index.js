38991f5d7d885658d3c72339f5afdcfbd8f5082d
mathcard <matheus.carmo@hotmail.com>
2019-12-22 23:33:15 -0300

    Realizando projeto mobile

---
+++ rocketSeat/mobile/src/pages/Main/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Text, View, Image, TextInput, TouchableOpacity } from 'react-native'
+import styles from './styles'; 
+
+//<image style={style.logo} source={} />
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+      </View>
+    );
+  }
+}
