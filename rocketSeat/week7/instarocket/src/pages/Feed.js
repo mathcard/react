e423f132e8cb0d81edcd5b2c6d79d8278a55a522
mathcard <matheus.carmo@hotmail.com>
2020-01-09 23:46:31 -0300

    evoluindo no mobile

--- rocketSeat/week7/instarocket/src/pages/Feed.js
+++ rocketSeat/week7/instarocket/src/pages/Feed.js
@@ -1,12 +1,81 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import api from '../services/api';
+import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+
+import camera from '../assets/camera.png';
+import more from '../assets/more.png';
+import like from '../assets/like.png';
+import comment from '../assets/comment.png';
+import send from '../assets/send.png';
 
 export default class Feed extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <TouchableOpacity style={{marginRight: 20}} onPress={()=> navigation.navigate('New')}>
+        <Image source={camera} />
+      </TouchableOpacity>
+    ),
+  });
+  
+
+  // Variavel dentro do componente para armazenar informações que serão alteradas  
+  state = {
+    feed : [],
+  };
+
+  // Executa automaticamente qdo componente e colocado em tela
+  async componentDidMount(){
+    //this.registerToSocket();
+
+    const response = await api.get('posts');
+
+    console.log(response.data);
+    // Alterando o valor da variavel com os dados da api
+    this.setState({ feed: response.data });
+  }
+
+
   render() {
     return (
-      <View>
-        <Text> Feed </Text>
+      <View style={styles.container}> 
+          <FlatList 
+          data={this.state.feed}
+          keyExtractor={post => post._id}
+          renderItem={({ item }) => (
+            <View style={styles.feedItem}> 
+              <View style={styles.feedItemHeader}>
+                <View style={styles.userInfo}>
+                  <Text style={styles.name}>{item.author}</Text>
+                  <Text style={styles.place}>{item.place}</Text>
+                </View>                
+              <Image source={more} />
+              </View>              
+            <Image style={styles.feedImage} source={{uri: `http://192.168.15.7:3333/${item.image}`}} />
+            <View style={styles.feedItemFooter}> 
+              <View style={styles.actions}>
+                <TouchableOpacity onPress={() => {}}>
+                  <Image source={like} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                  <Image source={comment} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                  <Image source={send} />
+                </TouchableOpacity>
+              </View>
+              <Text style={styles.likes}>{item.likes} curtidas</Text>
+              <Text style={styles.description}>{item.description}</Text>
+              <Text style={styles.hashtags}>{item.hashtags}</Text>
+            </View>
+          </View>
+
+          )}
+        />
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+
+});
--- rocketSeat/week7/instarocket/src/routes.js
+++ rocketSeat/week7/instarocket/src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+
 
 import Feed from './pages/Feed';
 import New from './pages/New';
 
-//import logo from './assets/logo.png';
+import logo from './assets/logo.png';
 
 export default createAppContainer(
   createStackNavigator({
@@ -14,7 +15,10 @@ export default createAppContainer(
     New,
   }, {
     defaultNavigationOptions: {
-      headerTitle: 'InstaRocket'
+      headerTintColor: '#000',
+      headerTitle: <Image style={{marginHorizontal: 80}} source={logo} />,
+      headerBackTitle: null,
     },
+    mode: 'modal' // Animação debaixo para cima
   })
 );  
