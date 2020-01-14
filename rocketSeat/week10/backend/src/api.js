233572ef5f3394238d8702cbaca76add84281203
mathcard <matheus.carmo@hotmail.com>
2020-01-14 17:55:01 -0300

    backend week 10

--- rocketSeat/week10/backend/src/api.js
+++ rocketSeat/week10/backend/src/api.js
@@ -4,11 +4,13 @@ const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
 /*
-{	
+{	Cadastrar dev: http://localhost:3333/devs -> Body:
 	"github_username": "mathcard",
 	"techs": "React Js, React Native, Node JS",
 	"latitude": -16.6851817 ,
 	"longitude": -49.2686083	
+
+    Listar Dev: http://localhost:3333/devs
 }
 
 */
