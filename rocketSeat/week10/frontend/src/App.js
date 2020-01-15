dcb84be4405781c6a722ce81d15c7aea92c42531
mathcard <matheus.carmo@hotmail.com>
2020-01-15 18:32:35 -0300

    Beggin FrontEnd Aplication - css

--- rocketSeat/week10/frontend/src/App.js
+++ rocketSeat/week10/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './global.css';
 import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
@@ -8,8 +9,28 @@ function App() {
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <label htmlFor=""> Usuário do Github</label> 
-          <input name="github_username" id="github_username" required/>
+          <div className="input-block">
+            <label htmlFor="github_username"> Usuário do Github</label> 
+            <input name="github_username" id="github_username" required/>
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs"> Tecnologias</label> 
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude"> Latitude</label> 
+              <input name="latitude" id="latitude" required/>
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="longitude"> Longitude </label> 
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+          
+          <button type="submit"> Salvar </button>
         </form>
       </aside>
       <main>
