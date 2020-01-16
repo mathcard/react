7bd8551c5c6f669cc2983034a5b216fee439fc8c
mathcard <matheus.carmo@hotmail.com>
2020-01-16 14:16:07 -0300

    Front End - latitude e longitude

--- rocketSeat/week10/frontend/src/App.js
+++ rocketSeat/week10/frontend/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [github_username, setGithub_username] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+
+  // Dispara uma função quando alguma informação alterar
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+       // console.log(position);
+      const { latitude, longitude} = position.coords;
+      setLatitude(latitude);
+      setLongitude(longitude);
+
+      },
+      (err)=> { 
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+
+    );
+  }, []);
+  
+  
+
   return (
     <div id="app">
       <aside>
@@ -11,22 +40,48 @@ function App() {
         <form>
           <div className="input-block">
             <label htmlFor="github_username"> Usuário do Github</label> 
-            <input name="github_username" id="github_username" required/>
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}              
+              onChange={ e=> setGithub_username(e.target.value)}
+            />
           </div>
           <div className="input-block">
             <label htmlFor="techs"> Tecnologias</label> 
-            <input name="techs" id="techs" required/>
+            <input 
+              name="techs" 
+              id="techs" 
+              required
+              value={techs}
+              onChange={ e=> setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude"> Latitude</label> 
-              <input name="latitude" id="latitude" required/>
+              <input 
+                type="number"
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
             
             <div className="input-block">
               <label htmlFor="longitude"> Longitude </label> 
-              <input name="longitude" id="longitude" required/>
+              <input 
+                type="number"
+                name="longitude"
+                id="longitude" 
+                required 
+                value={longitude} 
+                onChange={e => setLatitude(e.target.value)} 
+              />
             </div>
           </div>
           
@@ -34,7 +89,55 @@ function App() {
         </form>
       </aside>
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/28113945?s=460&v=4" alt="Matheus do Carmo"/>
+              <div className="user-info">
+                <strong>Matheus do Carmo</strong>
+                <span>React Js, React Native, Node.js</span>
+              </div>            
+            </header>
+            <p>Student developer in news tecnologies!</p>
+            <a href="https://github.com/mathcard">Acessar perfil</a>
+          </li>
 
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/28113945?s=460&v=4" alt="Matheus do Carmo"/>
+              <div className="user-info">
+                <strong>Matheus do Carmo</strong>
+                <span>React Js, React Native, Node.js</span>
+              </div>            
+            </header>
+            <p>Student developer in news tecnologies!</p>
+            <a href="https://github.com/mathcard">Acessar perfil</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/28113945?s=460&v=4" alt="Matheus do Carmo"/>
+              <div className="user-info">
+                <strong>Matheus do Carmo</strong>
+                <span>React Js, React Native, Node.js</span>
+              </div>            
+            </header>
+            <p>Student developer in news tecnologies!</p>
+            <a href="https://github.com/mathcard">Acessar perfil</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/28113945?s=460&v=4" alt="Matheus do Carmo"/>
+              <div className="user-info">
+                <strong>Matheus do Carmo</strong>
+                <span>React Js, React Native, Node.js</span>
+              </div>            
+            </header>
+            <p>Student developer in news tecnologies!</p>
+            <a href="https://github.com/mathcard">Acessar perfil</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
