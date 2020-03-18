1b78d868691434c101a96afbf2d1b024d22692a8
mathcard <matheus.carmo@hotmail.com>
2020-03-18 00:15:11 -0300

    Adicionando semana 6 e continuando semana 8.2

--- rocketSeat/week8/backend/src/server.js
+++ rocketSeat/week8/backend/src/server.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
-mongoose.connect('mongodb+srv://week8:week8@cluster0-bakma.mongodb.net/week8?retryWrites=true&w=majority', {
-  useNewUrlParser: true
-});
-
-
+mongoose.connect('mongodb+srv://week8:week8@cluster0-bakma.mongodb.net/week8?retryWrites=true&w=majority' , { useNewUrlParser: true });
 
 server.use(express.json());
 server.use(routes);
